8dbd08bb0aab6d3512a928168c14d01854362706
Boopathi <www.mboopathi@gmail.com>
2019-09-15 16:07:59 +0530

    Creating Files

--- NodeJS/Sample/first-app.js
+++ NodeJS/Sample/first-app.js
@@ -3,3 +3,5 @@ console.log("Hello from node js");
 
 const fs = require('fs');
 fs.writeFileSync('hello.txt','File insert from Node js');
+
+require('fs').writeFileSync('helo.txt','File insert from Node js');
